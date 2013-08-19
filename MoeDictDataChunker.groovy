97272dd0d542fa5dbbb82a3e4ad8ced40f6902c9
chyiro <chyiro@gmail.com>
2013-08-20 06:57:07 +0900

    unescape output json

--- MoeDictDataChunker.groovy
+++ MoeDictDataChunker.groovy
@@ -1,3 +1,4 @@
+
 @Grapes([
     @Grab(group='com.chenlb.mmseg4j', module='mmseg4j-core', version='1.9.1'),
     @Grab(group='com.chenlb.mmseg4j', module='mmseg4j-analysis', version='1.9.1'),
@@ -6,6 +7,7 @@
 
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
+import groovy.json.StringEscapeUtils
 
 import com.chenlb.mmseg4j.ComplexSeg
 import com.chenlb.mmseg4j.Dictionary
@@ -13,16 +15,22 @@ import com.chenlb.mmseg4j.MMSeg
 import com.chenlb.mmseg4j.Seg
 import com.chenlb.mmseg4j.Word
 
+
+/* input and output file */
+//File source = new File("moedict_data", "sample.json") // sample
+//File target = new File("moedict_data", "chunked_sample.json") // sample
+File source = new File("moedict_data", "dict-revised.json")
+File target = new File("moedict_data", "dict-revised-chunked.json")
+
 /* initialize mmseg4j */
 System.setProperty("mmseg.dic.path", "mmseg4j_data");
 Dictionary dic = Dictionary.getInstance()
 Seg seg = new ComplexSeg(dic)
 
-/**
- * Chunk given string to semicolon separated strings
- * @param str string to be chunked
- * @return result of chunking
- */
+/* separator for chunked strings */
+String SEP = ";"
+
+/* closure that does chunking */
 def chunk = { String str ->
     /* do chunking */
     MMSeg mmSeg = new MMSeg(new StringReader(str), seg)
@@ -31,22 +39,16 @@ def chunk = { String str ->
     def chunkedTokens = ""
     Word word = mmSeg.next()
     while (word) {
-        chunkedTokens += (word.getString() + ";")
+        chunkedTokens += (word.getString() + SEP)
         word = mmSeg.next()
     }
     chunkedTokens
 }
 
-/* container for chunked json */
+/* for each article in original file,
+ * chunk values for each key except for "type" and add result to container */
 def chunkedJsons = []
-
-/* read and parse original jsons */
-File source = new File("moedict_data", "sample.json") // sample
-//File source = new File("moedict_data", "dict-revised.json")
-def reader = source.newReader()
-
-/* for each article, chunk values for each key except for "type" and add result to container */
-new JsonSlurper().parse(reader).each { article ->
+new JsonSlurper().parse(source.newReader()).each { article ->
     def title = article.title
     article.heteronyms.definitions.each { entry ->
         entry.each { definition ->
@@ -62,13 +64,8 @@ new JsonSlurper().parse(reader).each { article ->
 /* get string representation of chunked result */
 def builder = new JsonBuilder()
 builder.call(chunkedJsons)
-def str = builder.toPrettyString()
+def str = StringEscapeUtils.unescapeJava(builder.toPrettyString())
 
 /* write to file */
-File target = new File("moedict_data", "chunked_sample.json") // sample
-//File target = new File("moedict_data", "dict-revised-chunked.json")
-def writer = target.newWriter()
-writer.write(str)
-writer.flush()
-writer.close()
+target.write(str)
 
